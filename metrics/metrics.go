62da50785ccabec6107e264a8a0a1b25aad30131
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2024-05-13 00:53:42 +0300

    metric
    Signed-off-by: Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,10 +1,7 @@
 package metrics
 
-import "go.opentelemetry.io/otel/sdk/metric"
-
 // otlp metrics
 func RegisterMetricExporter() error {
 
-	metric.NewMeterProvider(metric.WithReader())
 	return nil
 }

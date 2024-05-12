aa9a6efdb7c35ec72099650995477430e611c4c7
Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>
2024-05-13 00:43:12 +0300

    logging refactor
    Signed-off-by: Viacheslav Zalitskyi <viacheslav.zalitsky@gmail.com>

--- go.mod
+++ go.mod
@@ -15,7 +15,6 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
-	github.com/go-chi/traceid v0.2.0 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/uuid v1.6.0 // indirect
--- logging/logging.go
+++ logging/logging.go
@@ -10,52 +10,24 @@ import (
 	"time"
 )
 
-// Logger is a custom logger that logs messages using zerolog.Logger.
-type Logger struct {
-	logger *zerolog.Logger
-}
-
-func (l *Logger) UpdateContext(update func(c zerolog.Context) zerolog.Context) {
-	l.logger.UpdateContext(update)
-}
-func (l *Logger) Trace() *zerolog.Event                        { return l.logger.Trace() }
-func (l *Logger) Debug() *zerolog.Event                        { return l.logger.Debug() }
-func (l *Logger) Info() *zerolog.Event                         { return l.logger.Info() }
-func (l *Logger) Warn() *zerolog.Event                         { return l.logger.Warn() }
-func (l *Logger) Error() *zerolog.Event                        { return l.logger.Error() }
-func (l *Logger) Err(err error) *zerolog.Event                 { return l.logger.Err(err) }
-func (l *Logger) Fatal() *zerolog.Event                        { return l.logger.Fatal() }
-func (l *Logger) Panic() *zerolog.Event                        { return l.logger.Panic() }
-func (l *Logger) WithLevel(level zerolog.Level) *zerolog.Event { return l.logger.WithLevel(level) }
-func (l *Logger) Log() *zerolog.Event                          { return l.logger.Log() }
-func (l *Logger) Print(v ...interface{})                       { l.logger.Print(v...) }
-func (l *Logger) Printf(format string, v ...interface{})       { l.logger.Printf(format, v...) }
-func (l *Logger) Println(v ...interface{})                     { l.logger.Println(v...) }
-
 type OptionFunc func(zerolog.Context) zerolog.Context
 
 // NewDefaultLogger returns a global logger, which can be set once via New function.
 // If New wasn't called at least once, zerolog.Nop will be returned
-func NewDefaultLogger(w io.Writer) *Logger {
-	logger := zerolog.New(w)
-
-	l := &Logger{logger: &logger}
-
-	return l
+func NewDefaultLogger(w io.Writer) *zerolog.Logger {
+	l := zerolog.New(w)
+	return &l
 }
 
-func NewNopLogger() *Logger {
-	logger := zerolog.Nop()
-
-	l := &Logger{logger: &logger}
-
-	return l
+func NewNopLogger() *zerolog.Logger {
+	l := zerolog.Nop()
+	return &l
 }
 
-var logger *Logger
+var logger *zerolog.Logger
 
 // New initializes the logger. Sets the global logger once.
-func New(opts ...OptionFunc) *Logger {
+func New(opts ...OptionFunc) *zerolog.Logger {
 	if logger != nil {
 		return logger
 	}
@@ -79,8 +51,6 @@ func New(opts ...OptionFunc) *Logger {
 	for _, opt := range opts {
 		log.UpdateContext(opt)
 	}
-
-	logger = &Logger{logger: &log}
 
 	return logger
 }
